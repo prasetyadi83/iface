100ff73c3785c9cd20f0cc2b727c6fb44a3e1098
prasetyadi83 <prasetyadi83@gmail.com>
2020-01-05 21:39:54 +0700

    Updates

---
+++ precache-manifest.e95d05d04f6330abfdc143548b5fdc0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaed530bbe9d22d9a5f2a9a154009bf1",
+    "url": "/iface/index.html"
+  },
+  {
+    "revision": "d90209121ea8a4338467",
+    "url": "/iface/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "012f4b4f34fe54a0c28b",
+    "url": "/iface/static/css/main.01452e8e.chunk.css"
+  },
+  {
+    "revision": "d90209121ea8a4338467",
+    "url": "/iface/static/js/2.be138008.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/iface/static/js/2.be138008.chunk.js.LICENSE"
+  },
+  {
+    "revision": "012f4b4f34fe54a0c28b",
+    "url": "/iface/static/js/main.5da14508.chunk.js"
+  },
+  {
+    "revision": "dbebfaa25c7b867192e4",
+    "url": "/iface/static/js/runtime-main.8925f91c.js"
+  }
+]);
